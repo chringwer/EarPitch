ca2deb94eacd7dc490cde4ab225ff7705490dddc
Christoph Ingwersen <christoph.ingwersen@gmail.com>
2014-12-31 13:08:17 +0100

    feat(Keyboard): make Keyboard resizable

--- src/main/java/earpitch/widget/keyboard/Keyboard.java
+++ src/main/java/earpitch/widget/keyboard/Keyboard.java
@@ -2,55 +2,69 @@ package earpitch.widget.keyboard;
 
 import java.util.List;
 
+import javafx.collections.ObservableList;
 import javafx.scene.Cursor;
-import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import earpitch.Pitch;
 import earpitch.widget.keyboard.PitchMapper.Builder;
 
-public class Keyboard extends Group {
-	private final int width = 30;
-	private final int height = 100;
+public class Keyboard extends Region {
+    private final int width = 30;
+    private final int height = 150;
 
-	public Keyboard() {
-		Builder<Rectangle> mapper = new PitchMapper.Builder<Rectangle>();
+    public Keyboard() {
+        Builder<Rectangle> mapper = new PitchMapper.Builder<Rectangle>();
 
-		List<Rectangle> keys = mapper.groupBy(Pitch::toMidiNote)
-										.register('!', this::black)
-										.register('|', this::white)
-										.compile("|!||!|!||!|!")
-										.map(this::attachHandler);
+        List<Rectangle> keys = mapper.groupBy(Pitch::toMidiNote)
+                                     .register('!', this::black)
+                                     .register('|', this::white)
+                                     .compile("|!||!|!||!|!")
+                                     .map(this::attachHandler);
 
-		getChildren().addAll(keys);
-	}
+        getChildren().addAll(keys);
+    }
 
-	private Rectangle attachHandler(Rectangle key, List<Pitch> pitches) {
-		Pitch pitch = pitches.get(0);
-		key.setId("key-" + pitch.toMidiNote());
-		key.setOnMouseClicked((e) -> fireEvent(new NoteEvent(pitch)));
-		return key;
-	}
+    @Override
+    protected void layoutChildren() {
+        ObservableList<Node> children = getChildren();
+        double width = getWidth() / children.size();
 
-	private Rectangle black(int idx) {
-		Rectangle key = key(idx, Color.BLACK);
-		key.getStyleClass().add("black");
-		return key;
-	}
+        for (int idx = 0; idx < children.size(); idx++) {
+            Rectangle rectangle = (Rectangle) children.get(idx);
+            rectangle.setWidth(width);
+            rectangle.setTranslateX(idx * width);
+        }
+    }
 
-	private Rectangle key(int idx, Color fill) {
-		Rectangle key = new Rectangle(width, height);
-		key.setTranslateX(idx * width);
-		key.setStroke(Color.BLACK);
-		key.setFill(fill);
-		key.setCursor(Cursor.HAND);
-		key.getStyleClass().add("key");
-		return key;
-	}
+    private Rectangle attachHandler(Rectangle key, List<Pitch> pitches) {
+        Pitch pitch = pitches.get(0);
+        key.setId("key-" + pitch.toMidiNote());
+        key.setOnMouseClicked((e) -> fireEvent(new NoteEvent(pitch)));
+        return key;
+    }
 
-	private Rectangle white(int idx) {
-		Rectangle key = key(idx, Color.WHITE);
-		key.getStyleClass().add("white");
-		return key;
-	}
+    private Rectangle black(int idx) {
+        Rectangle key = key(idx, Color.BLACK);
+        key.getStyleClass().add("black");
+        return key;
+    }
+
+    private Rectangle key(int idx, Color fill) {
+        Rectangle key = new Rectangle(width, height);
+        key.setTranslateX(idx * width);
+        key.setStroke(Color.BLACK);
+        key.setFill(fill);
+        key.setCursor(Cursor.HAND);
+        key.getStyleClass().add("key");
+        return key;
+    }
+
+    private Rectangle white(int idx) {
+        Rectangle key = key(idx, Color.WHITE);
+        key.getStyleClass().add("white");
+        return key;
+    }
 }
